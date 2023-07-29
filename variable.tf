c0e47741125b0f9c820a380e0da5e07314ccdc51
Ayotunde <ayncode@gamail.com>
2023-07-29 00:18:29 +0000

    eks update

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "subnet_id_1" {
+  default = "subnet-e32dbcc2"
+  type= string
+
+}
+variable "subnet_id_2" {
+  default = "subnet-27c48829"
+  type = string
+}
+
+variable "cluster_name" {
+  type= string
+  default = "ay-eks-cluster"
+}
